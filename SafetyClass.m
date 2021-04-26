1126f50097eca1d5e7765e8fc9cc1acf827a946a
James <jameskruik@gmail.com>
2021-04-26 17:07:14 +1000

    Description of desired methods in SafetyClass

---
+++ SafetyClass.m
@@ -0,0 +1,46 @@
+classdef SafetyClass
+    %SAFETYCLASS Summary of this class goes here
+    %   Detailed explanation goes here
+    
+    properties
+        port = '/dev/ttyUSB0';
+        board = 'Uno';
+        a = arduino(port, board);
+        distaneGoal;
+        
+        
+    end
+    
+    methods
+        
+        function SetGoalDistance = SafetyClass()
+            
+            irReadings = zeros(1,initialIRreadings);
+            
+            for i = 1:initialIRreadings
+                irReadings(1,i) = readVoltage(a, 'A1')
+            end
+            
+            distanceGoal = mean(irReadings)
+            
+            function [bool] IsSafe = SafetyClass()
+                irRaw = readVoltage(a, 'A1');           % Analog pin 1 on arduino Uno
+                eStopRaw = readVoltage(a, 'A5');        % Analog pin 5 on arduino Uno
+                
+                if abs(distanceGoal-irRaw) > irThreshold
+                    curtainFlag = 1;        %something has crossed
+                end
+                
+                if eStopRaw >= 4 && curtainFlag == 0
+                    
+                    bool = True;
+
+                else
+                    bool = False;
+                end
+            end
+            
+        end
+    end
+end
+
