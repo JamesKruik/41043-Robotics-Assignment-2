64bcdc50c30668df229ee1ffca37867d694aa03f
James <jameskruik@gmail.com>
2021-05-01 08:29:09 +1000

    Simplified RMRC
    Put RMRC functionality into a single function, created an execution function for the qMatrix which checks collisions as well as the state of the safety devices

---
+++ ExecuteTrajectory.m
@@ -0,0 +1,39 @@
+function ExecuteTrajectory(robot, qMatrix, pauseValue, planePoints)
+%ANIMATETRAJ Summary of this function goes here
+%   Detailed explanation goes here
+    
+matrixSize = size(qMatrix);
+
+    for i = 1:1: matrixSize(1,1) 
+        % Update the next step in the traj
+        
+        
+        % Check the safety state
+        if CheckSafetyState == 1
+            disp('Safe to execute command');
+        else
+            disp('Unsafe to execute command - Check E-stop and Light Curtrain');
+        end
+        
+        
+        % Check collision avoidance using "planePoints" variable
+        if IsCollision == 0
+            disp('Safe - No collisions detected');
+        else
+            disp('Unsafe! Potential collision detected');
+            break
+        end
+        
+        
+        % If safe, send one step command to the robot
+
+        
+        robot.animate(qMatrix(i,:))
+            
+            pause(pauseValue);
+        
+    end
+end
+
+%execute - update one step, send one movement to the real robot, check
+    %   safety state
---
+++ GenerateRMRCTraj.m
@@ -0,0 +1,50 @@
+function [qMatrix] = GenerateRMRCTraj(robot,linearTraj,theta, steps, deltaT, epsilon, initialGuess, weightingMatrix)
+
+
+
+%GENERATERMRCTRAJ Summary of this function goes here
+%   %[qMatrix] = GenerateRMRCTraj(robot,linearTraj,theta, steps, deltaT, epsilon, initialGuess, weightingMatrix)
+
+
+T = [rpy2r(theta(1,1),theta(1,2),theta(1,3)) linearTraj(1,:)';zeros(1,3) 1]; % Create transformation of first point and angle
+
+q0 = zeros(1,6);
+qMatrix = zeros(steps,6);       % Array for joint anglesR% Initial guess for joint angles
+qMatrix(1,:) = robot.ikcon(T,initialGuess);
+
+for i = 1:steps-1
+    T = robot.fkine(qMatrix(i,:));                                           % Get forward transformation at current joint state
+    deltaX = linearTraj(i+1,:)' - T(1:3,4);                                  % Get position error from next waypoint
+    Rd = rpy2r(theta(1,1),theta(1,2),theta(1,3));                     % Get next RPY angles, convert to rotation matrix
+    Ra = T(1:3,1:3);                                                        % Current end-effector rotation matrix
+    Rdot = (1/deltaT)*(Rd - Ra);                                           % Calculate rotation matrix error
+    S = Rdot*Ra';                                                           % Skew symmetric!
+    linear_velocity = (1/deltaT)*deltaX;
+    angular_velocity = [S(3,2);S(1,3);S(2,1)];                              % Check the structure of Skew Symmetric matrix!!
+    %deltaTheta = tr2rpy(Rd*Ra');                                            % Convert rotation matrix to RPY angles
+    xdot = weightingMatrix*[linear_velocity;angular_velocity];                          	% Calculate end-effector velocity to reach next waypoint.
+    J = robot.jacob0(qMatrix(i,:));                 % Get Jacobian at current joint state
+    m(i) = sqrt(det(J*J'));
+    if m(i) < epsilon  % If manipulability is less than given threshold
+        lambda = (1 - m(i)/epsilon)*5E-2;
+    else
+        lambda = 0;
+    end
+    invJ = inv(J'*J + lambda *eye(6))*J';                                   % DLS Inverse
+    qdot(i,:) = (invJ*xdot)';                                                % Solve the RMRC equation (you may need to transpose the         vector)
+    for j = 1:6                                                             % Loop through joints 1 to 6
+        if qMatrix(i,j) + deltaT*qdot(i,j) < robot.qlim(j,1)                     % If next joint angle is lower than joint limit...
+            qdot(i,j) = 0; % Stop the motor
+        elseif qMatrix(i,j) + deltaT*qdot(i,j) > robot.qlim(j,2)                 % If next joint angle is greater than joint limit ...
+            qdot(i,j) = 0; % Stop the motor
+        end
+    end
+    qMatrix(i+1,:) = qMatrix(i,:) + deltaT*qdot(i,:);                         	% Update next joint state based on joint velocities
+    %positionError(:,i) = (linearTraj(i+1,:)' - T(1:3,4))';                     % For plotting
+    %angleError(:,i) = deltaTheta';                                           % For plotting
+end
+
+
+
+end
+
